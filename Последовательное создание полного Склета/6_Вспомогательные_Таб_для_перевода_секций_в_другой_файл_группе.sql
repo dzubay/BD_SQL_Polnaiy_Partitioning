ce02aca005967ef5026bfc3075acbe78a20352c8
dzubay <dzubay22112211@yandex.ru>
2025-02-06 21:56:06 +0300

    Добавление ещё скриптов, но пока с полнотекстовым до конца не решено, буду мучится ещё.

---
+++ Последовательное создание полного Склета/6_Вспомогательные_Таб_для_перевода_секций_в_другой_файл_группе.sql
@@ -0,0 +1,329 @@
+﻿use Magaz_DB_Poln_Test
+go
+ begin tran
+CREATE TABLE Employees_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Employee            bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+
+go
+CREATE TABLE Department_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Department          bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+
+go
+CREATE TABLE Group_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Group               bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+
+go
+CREATE TABLE The_Subgroup_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_The_Subgroup        bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+
+go
+CREATE TABLE Passport_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Passport            bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+
+go
+
+CREATE TABLE Post_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Post                bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+
+go
+CREATE TABLE Status_Employee_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Status_Employee     bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+
+go
+
+CREATE TABLE Connection_String_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Connection_String   bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+
+
+go
+
+CREATE TABLE Branch_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Branch              bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+
+go
+CREATE TABLE Country_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    Id_Country             bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+
+go
+
+CREATE TABLE Buyer_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    Id_buyer               bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Buyer_status_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    Id_Status              bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Condition_of_the_item_Audit_2
+(
+    AuditID                   bigint IDENTITY(1,1)  not null,
+    ID_Condition_of_the_item  bigint                null,
+ 	ModifiedBy                nVARCHAR(128)         null,
+    ModifiedDate              DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation                 CHAR(1)               null,
+    ChangeDescription         nvarchar(max)        null
+ --   PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Connection_Buyer_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Connection_Buyer    bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Currency_Audit_2
+(
+    AuditID              bigint IDENTITY(1,1)  not null,
+    ID_Currency          bigint                null,
+ 	ModifiedBy           nVARCHAR(128)         null,
+    ModifiedDate         DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation            CHAR(1)               null,
+    ChangeDescription    nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+)on PARTITION_Audit ;
+go
+CREATE TABLE Currency_Rate_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Currency_Rate       bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Data_Orders_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    Id_Data_Orders         bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Exemplar_Audit_2
+(
+    AuditID                   bigint IDENTITY(1,1)  not null,
+    ID_Exemplar               bigint                null,
+ 	ModifiedBy                nVARCHAR(128)         null,
+    ModifiedDate              DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation                 CHAR(1)               null,
+    ChangeDescription         nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Item_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    Id_Item                bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Orders_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Orders              bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+ --   PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Orders_status_Audit_2
+(
+    AuditID              bigint IDENTITY(1,1)  not null,
+    Id_Status            bigint                null,
+ 	ModifiedBy           nVARCHAR(128)         null,
+    ModifiedDate         DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation            CHAR(1)               null,
+    ChangeDescription    nvarchar(max)        null
+ --   PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Storage_location_Audit_2
+(
+    AuditID                   bigint IDENTITY(1,1)  not null,
+    ID_Storage_location       bigint                null,
+ 	ModifiedBy                nVARCHAR(128)         null,
+    ModifiedDate              DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation                 CHAR(1)               null,
+    ChangeDescription         nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE TRANSACTION_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Transaction         bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+ --   PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Transaction_status_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_Transaction_status  bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+ --   PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Type_of_product_measurement_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_product_measurement bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+ --   PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE Type_Storage_location_Audit_2
+(
+    AuditID                   bigint IDENTITY(1,1)  not null,
+    ID_Type_Storage_location  bigint                null,
+ 	ModifiedBy                nVARCHAR(128)         null,
+    ModifiedDate              DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation                 CHAR(1)               null,
+    ChangeDescription         nvarchar(max)        null
+ --   PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE TypeItem_Audit_2
+(
+    AuditID              bigint IDENTITY(1,1)  not null,
+    Id_TypeItem          bigint                null,
+ 	ModifiedBy           nVARCHAR(128)         null,
+    ModifiedDate         DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation            CHAR(1)               null,
+    ChangeDescription    nvarchar(max)        null
+ --   PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+CREATE TABLE TypeOrders_Audit_2
+(
+    AuditID                bigint IDENTITY(1,1)  not null,
+    ID_TypeOrders          bigint                null,
+ 	ModifiedBy             nVARCHAR(128)         null,
+    ModifiedDate           DATETIME              NOT NULL DEFAULT GETDATE(),
+	Operation              CHAR(1)               null,
+    ChangeDescription      nvarchar(max)        null
+--    PRIMARY KEY CLUSTERED ( AuditID ) 
+) on PARTITION_Audit;
+go
+
+--rollback
+commit
---
+++ Последовательное создание полного Склета/8_Созд_ПОЛНОТЕКСТОВЫХ_ИНДЕКСОВ_с_СХЕМОЙ_на_Табл_Audit_Audit_2.sql
@@ -0,0 +1,59 @@
+﻿
+--Полнотекстовые индексы создаются не на все таблицы, а на те которые я выбрал лично---------------------------------
+-----Так как в список не попали те, в которых нет много информации, и не должно быть---------------------------------
+
+
+use Magaz_DB_Poln_Test
+
+go
+--создание полнотекстового индекса
+CREATE FULLTEXT CATALOG CATALOG_FULLTEXT AS DEFAULT; 
+
+--Employees_Audit  
+--Employees_Audit_2
+CREATE FULLTEXT INDEX ON Employees_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Employees_Audit
+CREATE FULLTEXT INDEX ON Employees_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Employees_Audit_2
+--Passport_Audit  
+--Passport_Audit_2
+CREATE FULLTEXT INDEX ON Passport_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Passport_Audit
+CREATE FULLTEXT INDEX ON Passport_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Passport_Audit_2
+--Connection_String_Audit
+--Connection_String_Audit_2
+CREATE FULLTEXT INDEX ON Connection_String_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Connection_String_Audit
+CREATE FULLTEXT INDEX ON Connection_String_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Connection_String_Audit_2
+--Branch_Audit
+--Branch_Audit_2
+CREATE FULLTEXT INDEX ON Branch_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Branch_Audit
+CREATE FULLTEXT INDEX ON Branch_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Branch_Audit_2
+--Buyer_Audit
+--Buyer_Audit_2
+CREATE FULLTEXT INDEX ON Buyer_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Buyer_Audit
+CREATE FULLTEXT INDEX ON Buyer_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Buyer_Audit_2
+--Connection_Buyer_Audit
+--Connection_Buyer_Audit_2
+CREATE FULLTEXT INDEX ON Connection_Buyer_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Connection_Buyer_Audit
+CREATE FULLTEXT INDEX ON Connection_Buyer_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Connection_Buyer_Audit_2
+--Currency_Rate_Audit
+--Currency_Rate_Audit_2
+CREATE FULLTEXT INDEX ON Currency_Rate_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Currency_Rate_Audit
+CREATE FULLTEXT INDEX ON Currency_Rate_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Currency_Rate_Audit_2
+--Data_Orders_Audit
+--Data_Orders_Audit_2
+CREATE FULLTEXT INDEX ON Data_Orders_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Data_Orders_Audit
+CREATE FULLTEXT INDEX ON Data_Orders_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Data_Orders_Audit_2
+--Exemplar_Audit
+--Exemplar_Audit_2
+CREATE FULLTEXT INDEX ON Exemplar_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Exemplar_Audit
+CREATE FULLTEXT INDEX ON Exemplar_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Exemplar_Audit_2
+--Item_Audit
+--Item_Audit_2
+CREATE FULLTEXT INDEX ON Item_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Item_Audit
+CREATE FULLTEXT INDEX ON Item_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Item_Audit_2
+--Orders_Audit
+--Orders_Audit_2
+CREATE FULLTEXT INDEX ON Orders_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_Orders_Audit
+CREATE FULLTEXT INDEX ON Orders_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_Orders_Audit_2
+--TRANSACTION_Audit
+--TRANSACTION_Audit_2
+CREATE FULLTEXT INDEX ON TRANSACTION_Audit   (ChangeDescription) KEY INDEX index_UNIQUE_TRANSACTION_Audit
+CREATE FULLTEXT INDEX ON TRANSACTION_Audit_2 (ChangeDescription) KEY INDEX index_UNIQUE_TRANSACTION_Audit_2
